080b316c87582520b42c14f58c36b52872b108b5
Unknown <mpl0045@alu.ubu.es>
2018-02-25 20:47:51 +0100

    Formatting and indentation

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -21,16 +21,15 @@ import ubu.gii.dass.c01.ReusablePool;
  */
 public class ReusablePoolTest {
 
-	
 	ReusablePool reusablePool;
 	Reusable r1, r2, r3;
-	
+
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		reusablePool = ReusablePool.getInstance();		
+		reusablePool = ReusablePool.getInstance();
 	}
 
 	/**
@@ -49,63 +48,64 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testGetInstance() {
-		//fail("Not yet implemented");
-		
+
 		assertNotNull(reusablePool);
 		assertEquals(reusablePool, ReusablePool.getInstance());
 		assertTrue(reusablePool instanceof ReusablePool);
 	}
-	
+
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
+	 * 
 	 * @throws NotFreeInstanceException
 	 */
 	@Test
-	public void testAcquireReusable() throws NotFreeInstanceException{
-		//fail("Not yet implemented");
-		
+	public void testAcquireReusable() throws NotFreeInstanceException {
+
 		r1 = reusablePool.acquireReusable();
 		assertNotNull(r1);
 		assertTrue(r1 instanceof Reusable);
-		
+
 		r2 = reusablePool.acquireReusable();
 		assertNotNull(r2);
 		assertTrue(r2 instanceof Reusable);
-		
+
 		try {
 			r3 = reusablePool.acquireReusable();
 			assertNull(r3);
-		} catch (NotFreeInstanceException e) { }		 
-		
-		
+		} catch (NotFreeInstanceException e) {
+		}
+
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
-	 * @throws DuplicatedInstanceException 
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * 
+	 * @throws DuplicatedInstanceException
 	 */
 	@Test
 	public void testReleaseReusable() throws DuplicatedInstanceException {
-		//fail("Not yet implemented");
 		
 		try {
 			r1 = reusablePool.acquireReusable();
 			r2 = reusablePool.acquireReusable();
-		} catch (NotFreeInstanceException e1) {	}
-		
+		} catch (NotFreeInstanceException e1) {
+		}
+
 		// Test if releases a null object
 		reusablePool.releaseReusable(null);
-		
+
 		// Release both objects
 		reusablePool.releaseReusable(r1);
-		reusablePool.releaseReusable(r2);	
-		
+		reusablePool.releaseReusable(r2);
+
 		// Test a release of an already released object
 		try {
-			reusablePool.releaseReusable(r2);	
-		} catch (DuplicatedInstanceException e) { }
-		
-		
+			reusablePool.releaseReusable(r2);
+		} catch (DuplicatedInstanceException e) {
+		}
+
 	}
 
 }
