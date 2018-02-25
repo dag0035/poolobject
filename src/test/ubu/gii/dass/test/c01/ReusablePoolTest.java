b392851737b307799b62c7be5298f93990723c56
Daniel Arnaiz <dag0035@alu.ubu.es>
2018-02-25 16:09:09 +0100

    "testGetInstance" implementation
    "setUp" and "tearDown" implentation too.
    
    Co-Authored-By: mpl0045 <mpl0045@users.noreply.github.com>

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,17 +9,29 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
+
 /**
- * @author alumno
+ * @author Daniel Arnaiz
+ * @author Miguel Perez Lopez de Echazarreta
  *
  */
 public class ReusablePoolTest {
 
+	
+	ReusablePool reusablePool;
+	Reusable r1, r2;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		reusablePool = ReusablePool.getInstance();
+		//r1 = reusablePool.acquireReusable();
+		//r2 = reusablePool.acquireReusable();
+		
 	}
 
 	/**
@@ -27,6 +39,9 @@ public class ReusablePoolTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		reusablePool = null;
+		r1 = null;
+		r2 = null;
 	}
 
 	/**
@@ -34,9 +49,12 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		assertNotNull(reusablePool);
+		assertEquals(reusablePool, ReusablePool.getInstance());
+		assertTrue(reusablePool instanceof ReusablePool);
 	}
-
+	
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
